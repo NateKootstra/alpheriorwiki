984afff0f64f172e4569bca03d56b889f5675765
Nate Kootstra <natekootstra@gmail.com>
2025-06-21 15:24:46 -0400

    Optimizations and improvements.

--- main.py
+++ main.py
@@ -15,55 +15,55 @@ links = {
 replace = {
     "#X" : "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/X.png' )}}\"/></span>",
     
-    "#DPS": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/DPS.png' )}}\"/><span style=\"color: #ffffff; font-weight: 550;\">",
-    "#DMG": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/DMG.png' )}}\"/><span style=\"color: #ffa040; font-weight: 550;\">",
-    "#KB": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/KB.png' )}}\"/><span style=\"color: #40ffcf; font-weight: 550;\">",
-    "#PRJ": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/PRJ.png' )}}\"/><span style=\"color: #ffffff; font-weight: 550;\">",
-    "#FRT": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/FRT.png' )}}\"/><span style=\"color: #ff40cf; font-weight: 550;\">",
-    "#CRT": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/CRT.png' )}}\"/><span style=\"color: #ff7040; font-weight: 550;\">",
-    "#ACC": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/ACC.png' )}}\"/><span style=\"color: #40a0ff; font-weight: 550;\">",
-    "#RNG": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/RNG.png' )}}\"/><span style=\"color: #ffcf40; font-weight: 550;\">",
-    "#SPD": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/SPD.png' )}}\"/><span style=\"color: #40ff70; font-weight: 550;\">",
-    "#SZE": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/SZE.png' )}}\"/><span style=\"color: #cfff40; font-weight: 550;\">",
-    "#PWR": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/PWR.png' )}}\"/><span style=\"color: #ffffff; font-weight: 550;\">",
+    "#DPS": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/DPS.png' )}}\"/><span style=\"color: #ffffff; font-weight: bold;\">",
+    "#DMG": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/DMG.png' )}}\"/><span style=\"color: #ffa040; font-weight: bold;\">",
+    "#KB": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/KB.png' )}}\"/><span style=\"color: #40ffcf; font-weight: bold;\">",
+    "#PRJ": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/PRJ.png' )}}\"/><span style=\"color: #ffffff; font-weight: bold;\">",
+    "#FRT": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/FRT.png' )}}\"/><span style=\"color: #ff40cf; font-weight: bold;\">",
+    "#CRT": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/CRT.png' )}}\"/><span style=\"color: #ff7040; font-weight: bold;\">",
+    "#ACC": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/ACC.png' )}}\"/><span style=\"color: #40a0ff; font-weight: bold;\">",
+    "#RNG": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/RNG.png' )}}\"/><span style=\"color: #ffcf40; font-weight: bold;\">",
+    "#SPD": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/SPD.png' )}}\"/><span style=\"color: #40ff70; font-weight: bold;\">",
+    "#SZE": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/SZE.png' )}}\"/><span style=\"color: #cfff40; font-weight: bold;\">",
+    "#PWR": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/PWR.png' )}}\"/><span style=\"color: #ffffff; font-weight: bold;\">",
     
-    "#HP": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/HP.png' )}}\"/><span style=\"color: #ff4040; font-weight: 550;\">",
-    "#SP": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/SP.png' )}}\"/><span style=\"color: #4040ff; font-weight: 550;\">",
-    "#HM": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/MH.png' )}}\"/><span style=\"color: #40cfff; font-weight: 550;\">",
-    "#CR": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/CR.png' )}}\"/><span style=\"color: #40cfff; font-weight: 550;\">",
-    "#CD": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/CD.png' )}}\"/><span style=\"color: #40cfff; font-weight: 550;\">",
-    "#TS": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/SCL.png' )}}\"/><span style=\"color: #cfff40; font-weight: 550;\">",
-    "#MS": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/MS.png' )}}\"/><span style=\"color: #40ff70; font-weight: 550;\">",
-    "#RR": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/RR.png' )}}\"/><span style=\"color: #ffffff; font-weight: 550;\">",
+    "#HP": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/HP.png' )}}\"/><span style=\"color: #ff4040; font-weight: bold;\">",
+    "#SP": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/SP.png' )}}\"/><span style=\"color: #4040ff; font-weight: bold;\">",
+    "#HM": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/MH.png' )}}\"/><span style=\"color: #40cfff; font-weight: bold;\">",
+    "#CR": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/CR.png' )}}\"/><span style=\"color: #40cfff; font-weight: bold;\">",
+    "#CD": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/CD.png' )}}\"/><span style=\"color: #40cfff; font-weight: bold;\">",
+    "#TS": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/SCL.png' )}}\"/><span style=\"color: #cfff40; font-weight: bold;\">",
+    "#MS": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/MS.png' )}}\"/><span style=\"color: #40ff70; font-weight: bold;\">",
+    "#RR": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/RR.png' )}}\"/><span style=\"color: #ffffff; font-weight: bold;\">",
     
-    "#Explosive": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Explosive.png' )}}\"/><span style=\"color: #ffff00; font-weight: 550;\">",
-    "#Area": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Area.png' )}}\"/><span style=\"color: #dfff00; font-weight: 550;\">",
-    "#Bounce": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Bounce.png' )}}\"/><span style=\"color: #95aaaa; font-weight: 550;\">",
-    "#Burn": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Burn.png' )}}\"/><span style=\"color: #df8080; font-weight: 550;\">",
-    "#Pierce": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Pierce.png' )}}\"/><span style=\"color: #00ffff; font-weight: 550;\">",
-    "#Homing": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Homing.png' )}}\"/><span style=\"color: #ffff00; font-weight: 550;\">",
-    "#Sticky": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Sticky.png' )}}\"/><span style=\"color: #ff8080; font-weight: 550;\">",
-    "#Split": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Split.png' )}}\"/><span style=\"color: #95ff55; font-weight: 550;\">",
-    "#Flak": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Flak.png' )}}\"/><span style=\"color: #dfff00; font-weight: 550;\">",
-    "#Burst": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Burst.png' )}}\"/><span style=\"color: #8080ff; font-weight: 550;\">",
-    "#Proximity": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Proximity.png' )}}\"/><span style=\"color: #aaaaaa; font-weight: 550;\">",
-    "#Hitscan": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Hitscan.png' )}}\"/><span style=\"color: #00ffff; font-weight: 550;\">",
+    "#Explosive": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Explosive.png' )}}\"/><span style=\"color: #ffff00; font-weight: bold;\">",
+    "#Area": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Area.png' )}}\"/><span style=\"color: #dfff00; font-weight: bold;\">",
+    "#Bounce": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Bounce.png' )}}\"/><span style=\"color: #95aaaa; font-weight: bold;\">",
+    "#Burn": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Burn.png' )}}\"/><span style=\"color: #df8080; font-weight: bold;\">",
+    "#Pierce": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Pierce.png' )}}\"/><span style=\"color: #00ffff; font-weight: bold;\">",
+    "#Homing": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Homing.png' )}}\"/><span style=\"color: #ffff00; font-weight: bold;\">",
+    "#Sticky": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Sticky.png' )}}\"/><span style=\"color: #ff8080; font-weight: bold;\">",
+    "#Split": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Split.png' )}}\"/><span style=\"color: #95ff55; font-weight: bold;\">",
+    "#Flak": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Flak.png' )}}\"/><span style=\"color: #dfff00; font-weight: bold;\">",
+    "#Burst": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Burst.png' )}}\"/><span style=\"color: #8080ff; font-weight: bold;\">",
+    "#Proximity": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Proximity.png' )}}\"/><span style=\"color: #aaaaaa; font-weight: bold;\">",
+    "#Hitscan": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Hitscan.png' )}}\"/><span style=\"color: #00ffff; font-weight: bold;\">",
     
-    "#Microbe": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Microbe.png' )}}\"/><span style=\"color: #ff4070; font-weight: 550;\">",
-    "#Offspring": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Offspring.png' )}}\"/><span style=\"color: #ffa0b8; font-weight: 550;\">",
+    "#Microbe": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Microbe.png' )}}\"/><span style=\"color: #ff4070; font-weight: bold;\">",
+    "#Offspring": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Offspring.png' )}}\"/><span style=\"color: #ffa0b8; font-weight: bold;\">",
     
-    "#Biomass": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Biomass.png' )}}\"/><span style=\"color: #ffff40; font-weight: 550;\">",
-    "#Research": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Research.png' )}}\"/><span style=\"color: #bf80ff; font-weight: 550;\">",
+    "#Biomass": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Biomass.png' )}}\"/><span style=\"color: #ffff40; font-weight: bold;\">",
+    "#Research": "<img class=\"symbol\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/stats/Research.png' )}}\"/><span style=\"color: #bf80ff; font-weight: bold;\">",
     
-    "#Abyss": "<span style=\"color: #a040ff; font-weight: 550;\">",
-    "#Mark": "<span style=\"color: #ff4040; font-weight: 550;\">",
+    "#Abyss": "<span style=\"color: #a040ff; font-weight: bold;\">",
+    "#Mark": "<span style=\"color: #ff4040; font-weight: bold;\">",
     
-    "#B": "<span style=\"font-weight: 550\">",
+    "#B": "<span style=\"font-weight: bold\">",
     
-    "#A": "<img class=\"acgt\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/dna/A.png' )}}\"/><span style=\"color: #ff40ff; font-weight: 550;\">",
-    "#C": "<img class=\"acgt\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/dna/C.png' )}}\"/><span style=\"color: #40ffff; font-weight: 550;\">",
-    "#G": "<img class=\"acgt\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/dna/G.png' )}}\"/><span style=\"color: #a0ff40; font-weight: 550;\">",
-    "#T": "<img class=\"acgt\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/dna/T.png' )}}\"/><span style=\"color: #ffff40; font-weight: 550;\">",
+    "#A": "<img class=\"acgt\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/dna/A.png' )}}\"/><span style=\"color: #ff40ff; font-weight: bold;\">",
+    "#C": "<img class=\"acgt\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/dna/C.png' )}}\"/><span style=\"color: #40ffff; font-weight: bold;\">",
+    "#G": "<img class=\"acgt\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/dna/G.png' )}}\"/><span style=\"color: #a0ff40; font-weight: bold;\">",
+    "#T": "<img class=\"acgt\" style=\"display: inline;\" src=\"{{ url_for('static', filename='images/gg/dna/T.png' )}}\"/><span style=\"color: #ffff40; font-weight: bold;\">",
 }
 
 replaceFull = {
@@ -179,10 +179,10 @@ def get_template(path):
     for key in replaceFull.keys():
         template = template.replace("$" + key, replaceFull[key])
     for i in range(10):
-        template = template.replace("Turret Scale +" + str(i), "Turret Scale <span style='color: #fffff; font-weight: 550;'>+" + str(i))
-        template = template.replace("Turret Scale -" + str(i), "Turret Scale <span style='color: #fffff; font-weight: 550;'>-" + str(i))
-        template = template.replace(" +" + str(i), " <span style='color: #00ff00; font-weight: 550;'>+" + str(i))
-        template = template.replace(" -" + str(i), " <span style='color: #ff0000; font-weight: 550;'>-" + str(i))
+        template = template.replace("Turret Scale +" + str(i), "Turret Scale <span style='color: #fffff; font-weight: bold;'>+" + str(i))
+        template = template.replace("Turret Scale -" + str(i), "Turret Scale <span style='color: #fffff; font-weight: bold;'>-" + str(i))
+        template = template.replace(" +" + str(i), " <span style='color: #00ff00; font-weight: bold;'>+" + str(i))
+        template = template.replace(" -" + str(i), " <span style='color: #ff0000; font-weight: bold;'>-" + str(i))
         template = template.replace(str(i) + "%", str(i) + "%</span>")
     template = template.replace("$", "</span>")
     return render_template_string(template)
@@ -190,7 +190,7 @@ def get_template(path):
 # Public facing pages.
 @app.route("/")
 def index():
-    return redirect(request.url_root + "/gg/turrets/Guardian")
+    return redirect(request.url_root + "gg/turrets/Guardian")
 
 @app.route("/gg/turrets/<turretName>")
 def other_page(turretName):
---
+++ static/js/turret.js
@@ -0,0 +1,5 @@
+function showGrid() {
+    document.getElementsByClassName("turreticon")[0].style.display = "none";
+    document.getElementsByClassName("hamburger")[0].style.display = "none";
+    document.getElementsByClassName("turretlist")[0].style.display = "block";
+}
