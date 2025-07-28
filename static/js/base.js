cb6011d8b185b16d0d617902a3380f96b6944665
Nate Kootstra <natekootstra@gmail.com>
2025-07-28 01:52:01 -0400

    Remove overflow during loading screen.

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,9 @@
 window.onload = function () {
     Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
         document.getElementById("mainpage").style.visibility = "visible";
-        document.getElementsByClassName("loading")[0].remove()
+        document.getElementsByClassName("loading")[0].remove();
+
+        document.getElementsByTagName("html")[0].style.overflow = "visible";
+        document.getElementsByTagName("body")[0].style.overflow = "visible";
     })
 }
