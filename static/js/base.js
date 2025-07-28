d246c2b3944e96a31e932d40e9e66916602ee9dc
Nate Kootstra <natekootstra@gmail.com>
2025-07-28 12:25:08 -0400

    Inline base style and script.

--- static/js/base.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
-        document.getElementById("mainpage").style.visibility = "visible";
-        document.getElementsByClassName("loading")[0].remove();
-
-        document.getElementsByTagName("html")[0].style.overflow = "visible";
-        document.getElementsByTagName("body")[0].style.overflow = "visible";
-    })
-}
